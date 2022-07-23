8c639c4f7afa0d8c2e6cd0ca875db291a5a8d022
muhammadkhalid-dev <fiverfreelancing567@gmail.com>
2022-07-23 23:52:45 +0500

    initial commit

---
+++ src/app/constants.ts
@@ -0,0 +1,9 @@
+export const constants = {
+  API: {
+    GET_PLOTS_API: '/plot',
+    GET_PLOT_API: '/plot/{plotId}',
+    GET_CROPS_API: '/crop',
+    ADD_PLOT_API: '/plot',
+    EDIT_PLOT_API: '/plot/{plotId}'
+  }
+};
